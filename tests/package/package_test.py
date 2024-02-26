4a1a67b6af82e7ff0f73fe8064125f71cdc5b45d
Azmi SAHIN <azmisahin@outlook.com>
2024-02-26 04:46:03 +0000

    ✅: test the (Tracking) module startup control

--- tests/package/package_test.py
+++ tests/package/package_test.py
@@ -5,7 +5,7 @@ import unittest
 class TestPackage(unittest.TestCase):
     def test_Tracking_initialization(self):
         tracking_instance = Tracking()
-        result = tracking_instance.debug()
+        result = tracking_instance.debug
         self.assertTrue(result)  # Check if the result of a specific method is truthy
 
 
