931414650c6624b6da11d714b1fc40343a4aa9e1
Azmi SAHIN <azmisahin@outlook.com>
2024-03-06 11:34:08 +0300

    📦: updating compiled files or packages

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import setup, find_packages
 with open("README.md", "r") as fh:
     long_description = fh.read()
 setup(
-    name="web-package-template",
-    version="0.1",
+    name="web_package_template",
+    version="0.0.1",
     author="Azmi SAHIN",
     author_email="azmisahin@outlook.com",
     description="Project template for the development and production phases of Python packages.",
@@ -18,7 +18,13 @@ setup(
     url="https://github.com/azmisahin/azmisahin-software-web-package-template-pip-python-v1",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
+    python_requires=">=3.11.1",
+    entry_points={
+        "console_scripts": [
+            "tracker = package.app:main",
+        ],
+    },
     install_requires=[
         #
-    ],
+    ]
 )
